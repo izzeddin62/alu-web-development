fa54d516816519bd85666fe0622f9136243d6f88
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-14 22:05:37 +0200

    add second half 1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -15,3 +15,9 @@ class FIFOCache(BaseCaching):
             print("DISCARD: {}".format(discard))
             del self.cache_data[discard]
         self.cache_data[key] = item
+    
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
