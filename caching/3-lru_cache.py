9ca75d4890d315092f2dbbcfa8581df0cf3127a0
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-15 11:23:26 +0200

    modify 3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-"""lru cache"""
+""" lru cache """
 
 BaseCaching = __import__('base_caching').BaseCaching
 
-
-# class LRUCache(BaseCaching):
-#     """lru cache"""
-
-#     def put(self, key, item):
-#         """Add an item in the cache"""
-#         if key is None or item is None:
-#             return
-#         if key not in self.cache_data:
-#             if self.cache_data: 
-#                 discard = next(iter(self.cache_data))
-#                 print("DISCARD: {}".format(discard))
-#                 del self.cache_data[discard]
-#         self.cache_data[key] = item
-    
-#     def get(self, key):
-#         """Get an item in the cache"""
-#         if key is None or key not in self.cache_data:
-#             return None
-#         item = self.cache_data[key]
-#         del self.cache_data[key]
-#         self.cache_data[key] = item
-#         return self.cache_data[key]
-
-
 class LRUCache(BaseCaching):
     """lru cache"""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if self.cache_data: 
+                discard = next(iter(self.cache_data))
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
+        self.cache_data[key] = item
+    
+    def get(self, key):
+        """Get an item in the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        item = self.cache_data[key]
+        del self.cache_data[key]
+        self.cache_data[key] = item
+        return self.cache_data[key]
+
