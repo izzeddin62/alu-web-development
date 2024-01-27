bb29b593329abc44a35d6097d6d2cc481fa844d5
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-27 14:00:29 +0200

    resolve lint errors

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -6,6 +6,7 @@ Auth class
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """ Auth class
     """
@@ -13,14 +14,13 @@ class Auth:
         """ require_auth
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """ authorization_header
         """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user
         """
         return None
-    
