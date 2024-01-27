22ca64e407c11299dbd8aa5a6403ca59ed8a90d7
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-27 18:52:42 +0200

    resolve lint error

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,8 +9,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class
     """
-    
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
         """extract base64 auth header"""
         if authorization_header is None:
             return None
