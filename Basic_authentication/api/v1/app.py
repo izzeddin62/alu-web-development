87a5b6cb1faef911b2ece881b063e57c7fda1fa5
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-27 18:40:59 +0200

    resolve lint error

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -10,8 +10,6 @@ import os
 from api.v1.auth.auth import Auth
 
 
-
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -19,6 +17,7 @@ auth = None
 
 if os.getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -40,6 +39,7 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
 @app.before_request
 def before_request_func():
     """ Before request handler
@@ -57,6 +57,7 @@ def before_request_func():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
