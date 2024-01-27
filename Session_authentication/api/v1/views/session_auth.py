e44457fb5ad0d4560900f7f916bdde0a834d763a
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-27 21:40:40 +0200

    modify 7

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -19,15 +19,13 @@ def login() -> str:
     if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
     user = User.search({'email': email})
-    if len(user) == 0:
+    if not user or len(user) == 0:
         return jsonify({"error": "no user found for this email"}), 404
     user = user[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
-    print(user, '======')
-    print(user.to_json(), '======')
     response = jsonify(user.to_json())
     response.set_cookie(getenv('SESSION_NAME'), session_id)
-    return user.to_json(), 200
+    return response
