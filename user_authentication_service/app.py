442f485ce327d2b838a148e3a1643e6073ad2101
izzeddin62 <i.ishimwe@alustudent.com>
2024-02-04 15:51:58 +0200

    resolve lint errors

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_world():
     """hello world"""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
