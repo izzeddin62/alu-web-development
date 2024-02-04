ae8974bc83fe9a9106e46be78807fb4e5a622ee0
izzeddin62 <i.ishimwe@alustudent.com>
2024-02-04 15:35:05 +0200

    add 4

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" auth module """
+
+import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """hash password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
