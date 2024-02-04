a74b52f914bc8dc98fc5156d7379930547b62bbf
izzeddin62 <i.ishimwe@alustudent.com>
2024-02-04 22:55:11 +0200

    add 17

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -65,5 +65,16 @@ def profile():
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """get reset password token"""
+    email = request.form.get("email")
+    token = AUTH.get_reset_password_token(email)
+    try:
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
