982bca0885cca2a8f987dae9d8f339ba6f8dc916
izzeddin62 <i.ishimwe@alustudent.com>
2024-02-04 15:36:18 +0200

    resolve lint errors

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,4 +6,7 @@ import bcrypt
 
 def _hash_password(password: str) -> str:
     """hash password"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    return bcrypt.hashpw(
+        password.encode('utf-8'),
+        bcrypt.gensalt()
+        ).decode('utf-8')
